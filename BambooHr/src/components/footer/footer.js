c2f19125cda3963d20ddaf615b8b47331efa6617
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-14 20:43:19 -0600

    Footer
    Setting up grid and color to the footer component.

--- BambooHr/src/components/footer/footer.js
+++ BambooHr/src/components/footer/footer.js
@@ -5,61 +5,58 @@ export default class FooterContainer extends Component {
        return(
           <div className='footer-container'>
               <div className='footer-wrapper'>
-                    <div className='footer-top'>
-                        <div className='footer-company'>
+                    <div className='footer-left'>
                             <h1>Company</h1>
-                            <div className='footer-company__left'>
-                                <a>About Us</a>
-                                <a>Careers</a>
-                                <a>Why BambooHR</a>
-                                <a>Blog</a>
-                                <a>Contact Us</a>
+                            <div className='footer-left-links'>
+                                <div className='footer-left-links__left'>
+                                    <a className='footer-link'>About Us</a>
+                                    <a className='footer-link'>Careers</a>
+                                    <a className='footer-link'>Why BambooHR</a>
+                                    <a className='footer-link'>Blog</a>
+                                    <a className='footer-link'>Contact Us</a>
+                                </div>
+                                <div className='footer-left-links__right'>
+                                    <a className='footer-link'>Customers</a>
+                                    <a className='footer-link'>Events Calander</a>
+                                    <a className='footer-link'>HR Glossary</a>
+                                    <a className='footer-link'>Small Business</a>
+                                    <a className='footer-link'>HRIS</a>
+                                </div>
                             </div>
-                            <div className='footer-company__right'>
-                                <a>Customers</a>
-                                <a>Events Calander</a>
-                                <a>HR Glossary</a>
-                                <a>Small Business</a>
-                                <a>HRIS</a>
+                    </div>
+
+                    <div className='footer-middle'>
+                        <h1>Support</h1>
+                        <div className='footer-middle-links'>
+                            <div className='footer-middle-links__left'>
+                                <a className='footer-link'>Resources</a>
+                                <a className='footer-link'>Webinars</a>
+                                <a className='footer-link'>Services</a>
+                                <a className='footer-link'>FAQ</a>
+                                <a className='footer-link'>Partner with Us</a>
+                                <a className='footer-link'>API</a>
+                            </div>
+
+                            <div className='footer-middle-links__right'>
+                                <a className='footer-link'>App Marketplace</a>
+                                <a className='footer-link'>Login</a>
                             </div>
                         </div>
+                    </div>
 
-                        <div className='footer-support'>
-                            <h1>Support</h1>
-                            <div className='footer-support__left'>
-                                <a>Resources</a>
-                                <a>Webinars</a>
-                                <a>Services</a>
-                                <a>FAQ</a>
-                                <a>Partner with Us</a>
-                                <a>API</a>
-                            </div>
-                            <div className='footer-support__right'>
-                                <a>App Marketplace</a>
-                                <a>Login</a>
-                            </div>
-
-                        </div>
-
+                    <div className='footer-right'>
                         <div className='footer-subscribe'>
                             <h1>Subscribe to our Newsletter</h1>
                         </div>
-                    </div>
 
-                    <div className='footer-bottom'>
-                        <div className='footer-bottom__left'> 
-                            <div className='footer-bottom__social'>
+                        <div className='footer-bottom__social'>
+                            Social media Icons
+                        </div> 
 
-                            </div>
-                        </div>
-                        <div className='footer-bottom__right'> 
-                            <div className="footer-bottom__copy-right">
-                            © 2020 My Content LLC. All Rights Reserved. My Content® is a registered trademark of My Content LLC
-                            </div>
+                        <div className="footer-bottom__copy-right">
+                        © 2020 My Content LLC. All Rights Reserved. My Content® is a registered trademark of My Content LLC
                         </div>
                     </div>
-
-                    <div className='footer-opt-out'>Some content</div>
 
                 </div>
           </div>
