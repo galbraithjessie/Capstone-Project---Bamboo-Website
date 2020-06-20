f084223c1ba753a7bd63a9ae463ac7397a68f96a
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-20 10:42:58 -0600

    Packaging Component
    Set up inital page, and layout. Will be creating components in page after all pages are set up

--- BambooHr/src/components/app.js
+++ BambooHr/src/components/app.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavigationContainer from './navigation/navigation-container';
 import FooterContainer from './footer/footer';
 import Home from './pages/home';
-import Packages from './pages/packages';
+import Packaging from './pages/packaging';
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
@@ -19,7 +19,7 @@ export default class App extends Component {
               <Switch>
                 <Route exact path="/" component={Home} />
 
-                <Route exact path="/packages" component={Packages} />
+                <Route exact path="/packages" component={Packaging} />
 
                 <Route component={NoMatch} />
               </Switch>
--- BambooHr/src/components/pages/packaging.js
+++ BambooHr/src/components/pages/packaging.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
  
-export default class Packages extends Component {
+export default class Packaging extends Component {
    render() {
        return(
            //2 packages
@@ -9,8 +9,8 @@ export default class Packages extends Component {
           <div className='packages-container'>
               <div className="packages__heading">
                 <h1>Packaging</h1>
-                <ul>Wherever you are in your journey, BambooHR® has a package that’s right for you.</ul>
-                <ul>Choosing your HR software platform just got a whole lot easier. Our specially designed packages give you the right tools to reach your goal, whether that’s eliminating spreadsheets or becoming a strategic force in your company.</ul>
+                <h2>Wherever you are in your journey, BambooHR® has a package that’s right for you.</h2>
+                <p>Choosing your HR software platform just got a whole lot easier. Our specially designed packages give you the right tools to reach your goal, whether that’s eliminating spreadsheets or becoming a strategic force in your company.</p>
                 <button>Get a Free Quote</button>
               </div>
               <div className='packages'>
