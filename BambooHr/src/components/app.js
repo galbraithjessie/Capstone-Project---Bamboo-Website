51fbef4e7de04be8160e94d368bab43d9d11764e
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-20 12:19:20 -0600

    Created Pages
    Created initial pages and a Chat Widget. Styled the Chat Widget with scss to match website colors.

--- BambooHr/src/components/app.js
+++ BambooHr/src/components/app.js
@@ -4,8 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavigationContainer from './navigation/navigation-container';
 import FooterContainer from './footer/footer';
 import Home from './pages/home';
+import LogIn from './pages/logIn';
 import Packaging from './pages/packaging';
+import Blog from './pages/blog';
+import AboutUs from './pages/about';
+import ContactUs from './pages/contact';
+import SignUp from './pages/signUp';
 import NoMatch from "./pages/no-match";
+
+import ChatWidget from './widgets/chatWidget';
 
 export default class App extends Component {
   render() {
@@ -13,13 +20,19 @@ export default class App extends Component {
       <div className='container'>
         <Router>
           <div>
-              <NavigationContainer
-              />
+              <NavigationContainer />
+              <ChatWidget />
 
               <Switch>
                 <Route exact path="/" component={Home} />
 
                 <Route exact path="/packages" component={Packaging} />
+                <Route exact path="/lognin" component={LogIn} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route exact path="/blog" component={Blog} />
+                <Route exact path="/about" component={AboutUs} />
+                <Route exact path="/contact" component={ContactUs} />
+                
 
                 <Route component={NoMatch} />
               </Switch>
