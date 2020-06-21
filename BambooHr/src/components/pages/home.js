23db224cb7c0b3a4c4a9c2844c29cffb7555fda4
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-20 20:35:52 -0600

    Set up new Node Modules
    Set up some node modules to have extra component features. Finished home page to have a YouTube video auto play. Started blog component.

--- BambooHr/src/components/pages/home.js
+++ BambooHr/src/components/pages/home.js
@@ -1,14 +1,20 @@
 import React from "react";
-
-import bambooBackGround from '../../../static/assets/images/bambooBG.jpg';
+import YouTube from '@u-wave/react-youtube';
 
 export default function() {
   return (
     <div className='home-container'>
-        <div className='home-img'
-            style={{
-            backgroundImage: `url(${bambooBackGround})`
-        }} />
+      <div className='home-title'>Our Software</div>
+      <YouTube className='home-video'
+        video="7WraxuTDqk0"
+        autoplay
+        height='460'
+        width='840'
+      />
+
+      <div className='home-form'>
+        <button className='btn'>Get A Free Quote</button>
+      </div>
     </div>
   );
 }
