7474237fb53fefd5b2fcdfbffc33352bb16a668d
Jessie Galbraith <galbraithjessie@gmail.com>
2020-09-13 11:38:58 -0600

    Created content State for Edit mode
    Set up state for content to populate in the edit mode.

--- BambooHr/src/components/forms/rich-text-editor.js
+++ BambooHr/src/components/forms/rich-text-editor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
@@ -15,12 +15,22 @@ export default class RichTextEditor extends Component {
         this.onEditorStateChange = this.onEditorStateChange.bind(this);
     }
 
+    componentWillMount() {
+        if (this.props.editMode && this.props.contentToEdit) {
+            const blocksFromHtml = htmlToDraft(this.props.contentToEdit);
+            const { contentBlocks, entityMap } = blocksFromHtml;
+            const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
+            const editorState = EditorState.createWithContent(contentState);
+            this.setState({ editorState });
+        }
+    }
+
     onEditorStateChange(editorState) {
         this.setState(
             { editorState }, 
             this.props.handleRichTextEditorChange(
                 draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
-                )
+            )
         );
         
     }
